6ade08c99fb83e4d2d54755fa483996471550dd7
totoroyyw <953834564@qq.com>
2020-04-04 22:56:06 +0800

    lab2: move to parts, add stderr and todo ">>, x|y"

---
+++ lab2/sh.h
@@ -0,0 +1,37 @@
+#ifndef TOTORO_SH
+#define TOTORO_SH
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/signal.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <pwd.h>
+#define BUFF_SZ 256
+
+const char *_CD = "cd";
+const char *_RIN = "<";
+const char *_ROUT = ">";
+const char *_PIPE = "|";
+enum
+{
+    OK = 0,
+    ERROR_FORK,
+    ERROR_COMMAND,
+    ERROR_WRONG_PARAMETER,
+    ERROR_MISS_PARAMETER,
+    ERROR_TOO_MANY_PARAMETER,
+    ERROR_CD,
+    ERROR_EXIT,
+
+    ERROR_IN,
+    ERROR_OUT,
+
+    ERROR_PIPE,
+    ERROR_PIPE_MISS_PARAMETER
+};
+
+#endif
--- lab2/shell.cpp
+++ lab2/shell.cpp
@@ -1,226 +1,367 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <errno.h>
+#ifndef TOTORO_SH
+#include "sh.h"
+#endif
 
+int result;
+char argvs[BUFF_SZ][BUFF_SZ];
+
+int ELF_(const char *cmd)
+{
+	if (cmd == 0 || strlen(cmd) == 0)
+		return 0;
+	int END_ = 1;
+	int fSTD[2];
+	if (pipe(fSTD) == -1)
+	{
+		END_ = 0;
+	}
+	else
+	{
+		int inSTD = dup(STDIN_FILENO);
+		int outSTD = dup(STDOUT_FILENO);
+		int pid = vfork();
+		if (pid == -1)
+		{
+			END_ = 0;
+		}
+		else if (pid == 0)
+		{
+			close(fSTD[0]);
+			dup2(fSTD[1], STDOUT_FILENO);
+			close(fSTD[1]);
+			char buf[BUFF_SZ];
+			sprintf(buf, "command -v %s", cmd);
+			system(buf);
+			exit(1);
+		}
+		else
+		{
+			waitpid(pid, 0, 0);
+			close(fSTD[1]);
+			dup2(fSTD[0], STDIN_FILENO);
+			close(fSTD[0]);
+			if (getchar() == EOF)
+			{
+				END_ = 0;
+			}
+			dup2(inSTD, STDIN_FILENO);
+			dup2(outSTD, STDOUT_FILENO);
+		}
+	}
+	return END_;
+}
+int spCMD(char cmd[BUFF_SZ])
+{
+	int i, j, k = 0, len = strlen(cmd);
+	for (i = 0, j = 0; i < len; ++i)
+	{
+		if (cmd[i] != ' ')
+		{
+			argvs[k][j++] = cmd[i];
+		}
+		else
+		{
+			if (j != 0)
+			{
+				argvs[k][j] = '\0'; // PIPE |  yiding yao kongge?
+				k++;
+				j = 0;
+			}
+		}
+	}
+	if (j != 0)
+	{
+		argvs[k][j] = '\0';
+		k++;
+	}
+	return k;
+}
+int RED_CMD(int left, int right)
+{
+	if (!ELF_(argvs[left]))
+	{
+		return ERROR_COMMAND;
+	}
+	int inNum = 0, outNum = 0;
+	char *inFile = 0, *outFile = 0;
+	int endIndex = right;
+
+	for (int i = left; i < right; ++i)
+	{
+		if (strcmp(argvs[i], _RIN) == 0)
+		{
+			++inNum;
+			if (i + 1 < right)
+			{
+				inFile = argvs[i + 1];
+			}
+			else
+			{
+				return ERROR_MISS_PARAMETER;
+			}
+			if (endIndex == right)
+			{
+				endIndex = i;
+			}
+		}
+		else if (strcmp(argvs[i], _ROUT) == 0) // todo >>
+		{
+			outNum++;
+			if (i + 1 < right)
+			{
+				outFile = argvs[i + 1];
+			}
+			else
+			{
+				return ERROR_MISS_PARAMETER;
+			}
+			if (endIndex == right)
+			{
+				endIndex = i;
+			}
+		}
+	}
+	if (inNum == 1)
+	{
+		FILE *fp = fopen(inFile, "r");
+		if (fp == 0)
+		{
+			return ERROR_IN;
+		}
+		fclose(fp);
+	}
+	if (inNum > 1)
+	{
+		return ERROR_IN;
+	}
+	else if (outNum > 1)
+	{
+		return ERROR_OUT;
+	}
+	int END_ = OK;
+	int pid = vfork();
+	if (pid == -1)
+	{
+		END_ = ERROR_FORK;
+	}
+	else if (pid == 0)
+	{
+		if (inNum == 1)
+		{
+			freopen(inFile, "r", stdin);
+		}
+		if (outNum == 1)
+		{
+			freopen(outFile, "w", stdout);
+		}
+		char *comm[BUFF_SZ];
+		for (int i = left; i < endIndex; ++i)
+		{
+			comm[i] = argvs[i];
+		}
+		comm[endIndex] = 0;
+		execvp(comm[left], comm + left);
+		exit(errno);
+	}
+	else
+	{
+		int status;
+		waitpid(pid, &status, 0);
+		int err = WEXITSTATUS(status);
+		if (err)
+		{
+			printf("%s\n", strerror(err));
+		}
+	}
+	return END_;
+}
+int PIPE_CMD(int left, int right)
+{
+	if (left >= right)
+	{
+		return OK;
+	}
+	int pipeIndex = -1;
+	for (int i = left; i < right; ++i)
+	{
+		if (strcmp(argvs[i], _PIPE) == 0)
+		{
+			pipeIndex = i;
+			break;
+		}
+	}
+	if (pipeIndex == -1)
+	{
+		return RED_CMD(left, right);
+	}
+	else if (pipeIndex + 1 == right)
+	{
+		return ERROR_PIPE_MISS_PARAMETER;
+	}
+	int fSTD[2];
+	if (pipe(fSTD) == -1)
+	{
+		return ERROR_PIPE;
+	}
+	int END_ = OK;
+	int pid = vfork();
+	if (pid == -1)
+	{
+		END_ = ERROR_FORK;
+	}
+	else if (pid == 0)
+	{
+		close(fSTD[0]);
+		dup2(fSTD[1], STDOUT_FILENO);
+		close(fSTD[1]);
+		END_ = RED_CMD(left, pipeIndex);
+		exit(END_);
+	}
+	else
+	{
+		int status;
+		waitpid(pid, &status, 0);
+		int exitCode = WEXITSTATUS(status);
+		if (exitCode != OK)
+		{
+			char info[4096] = {0};
+			char line[BUFF_SZ];
+			close(fSTD[1]);
+			dup2(fSTD[0], STDIN_FILENO);
+			close(fSTD[0]);
+			while (fgets(line, BUFF_SZ, stdin) != 0)
+			{
+				strcat(info, line);
+			}
+			printf("%s", info);
+			END_ = exitCode;
+		}
+		else if (pipeIndex + 1 < right)
+		{
+			close(fSTD[1]);
+			dup2(fSTD[0], STDIN_FILENO);
+			close(fSTD[0]);
+			END_ = PIPE_CMD(pipeIndex + 1, right);
+		}
+	}
+	return END_;
+}
+int EXIT_()
+{
+	int pid = getpid();
+	if (kill(pid, SIGTERM) == -1)
+	{
+		return ERROR_EXIT;
+	}
+	else
+	{
+		return OK;
+	}
+}
+int CALL_(int commandNum)
+{
+	int pid = fork();
+	if (pid == -1)
+	{
+		return ERROR_FORK;
+	}
+	else if (pid == 0)
+	{
+		int inFds = dup(STDIN_FILENO);
+		int outFds = dup(STDOUT_FILENO);
+		int END_ = PIPE_CMD(0, commandNum);
+		dup2(inFds, STDIN_FILENO);
+		dup2(outFds, STDOUT_FILENO);
+		exit(END_);
+	}
+	else
+	{
+		int status;
+		waitpid(pid, &status, 0);
+		return WEXITSTATUS(status);
+	}
+}
 void sighandler(int signum)
 {
-	// keep alive
+	// keep alive, todo ctrl+d
 }
-#define BUFF_SZ 256
-#define CMD_SZ 128
-enum
-{
-	RESULT = 0,
-	ERROR_FORK,
-	ERROR_COMMAND,
-	ERROR_TOO_MANY_PARAMETER,
-
-	ERROR_MANY_IN,
-	ERROR_MANY_OUT,
-
-	ERROR_PIPE,
-
-	ERROR_EXIT,
-};
-
-int splitCMD(char cmd[BUFF_SZ], char *argvs[CMD_SZ])
-{
-	int i;
-	argvs[0] = cmd;
-	for (i = 0; *argvs[i]; i++)
-		for (argvs[i + 1] = argvs[i] + 1; *argvs[i + 1]; argvs[i + 1]++)
-			if (*argvs[i + 1] == ' ')
-			{
-				*argvs[i + 1] = '\0';
-				argvs[i + 1]++;
-				break;
-			}
-	argvs[i] = 0;
-}
-
 int main()
 {
 	signal(SIGINT, sighandler);
-	char cmd[BUFF_SZ];	 //input
-	char *argvs[CMD_SZ]; //argvs end in NULL
-	int i, pid, stat_val, isRedirect, isPipe, isBack, position, argc;
+	char argv[BUFF_SZ];
 	while (1)
 	{
-		isRedirect = 0;
-		isPipe = 0;
-		position = 0;
-		printf("# ");
-		fflush(stdin);
-		fgets(cmd, BUFF_SZ, stdin);
-		i = 0;
-		while (cmd[i] != '\n')
+		printf("➜  ");
+		fgets(argv, BUFF_SZ, stdin);
+		int len = strlen(argv);
+		if (len != BUFF_SZ)
 		{
-			i++;
+			argv[len - 1] = '\0';
 		}
-		cmd[i] = '\0';
-		splitCMD(cmd, argvs);
-
-		for (i = 0; argvs[i] != NULL; i++)
+		int argc = spCMD(argv);
+		if (argc != 0)
 		{
-			if (!strcmp(argvs[i], "<") || !strcmp(argvs[i], ">"))
+			if (strcmp(argvs[0], "exit") == 0)
 			{
-				isRedirect = 1;
-				position = i;
+				result = EXIT_();
+				if (result == ERROR_EXIT)
+				{
+					exit(-1);
+				}
 			}
-			if (!strcmp(argvs[i], "|"))
+			else if (strcmp(argvs[0], _CD) == 0)
 			{
-				isPipe = 1;
-				position = i;
-			}
-		}
-		//a lot of pipe????
-
-		if (!argvs[0])
-			continue;
-
-		if (strcmp(argvs[0], "cd") == 0)
-		{
-			if (argvs[1])
-			{
-				chdir(argvs[1]);
-			}
-			else
-			{
-				return ERROR_COMMAND;
-			}
-			continue;
-		}
-		if (strcmp(argvs[0], "pwd") == 0)
-		{
-			char wd[4096];
-			if (getcwd(wd, 4096) == nullptr)
-			{
-				puts(wd);
-			}
-			else
-			{
-				return ERROR_COMMAND;
-			}
-			continue;
-		}
-		if (strcmp(argvs[0], "export") == 0)
-		{
-			for (i = 1; argvs[i] != NULL; i++)
-			{
-				char *name = argvs[i];
-				char *value = argvs[i] + 1;
-				while (*value != '\0' && *value != '=')
-					value++;
-				*value = '\0';
-				value++;
-				setenv(name, value, 1);
-			}
-			continue;
-		}
-		if (strcmp(argvs[0], "exit") == 0)
-		{
-			pid = getpid();
-			if (kill(pid, SIGTERM) == -1)
-			{
-				return ERROR_EXIT;
-			}
-			else
-			{
-				return RESULT;
-			}
-		}
-
-		if (strcmp(argvs[i - 1], "&") == 0)
-		{
-			argvs[i - 1] = NULL;
-			pid = fork(); //bulid a runback process,and don't eliminate the parent process
-			if (pid == -1)
-			{
-				perror("Failed to fork");
-				return 1;
-			}
-			if (pid == 0)
-			{
-				if (setsid() == -1)
-					perror("Child faild to become a session leader");
+				if (argc < 2)
+				{
+					result = ERROR_MISS_PARAMETER;
+					fprintf(stderr, "Error miss parameter");
+				}
+				else if (argc > 2)
+				{
+					result = ERROR_TOO_MANY_PARAMETER;
+					fprintf(stderr, "Error too many parameter");
+				}
 				else
 				{
-					execvp(argvs[0], argvs);
-					printf("Command not found in PATH\n");
-					exit(0);
-				}
-				return 1;
-			}
-			continue;
-		}
-		if (isPipe)
-		{
-			pid_t child, gChild;
-			int file_pipes[2];
-			char **command;
-			command = &argvs[position + 1];
-			argvs[position] = NULL;
-			child = fork();
-			switch (child)
-			{
-			case -1:
-				perror("Fork Failed\n");
-				exit(-1);
-
-			case 0:
-				if (!pipe(file_pipes))
-				{
-					gChild = fork();
-					switch (gChild)
+					if (chdir(argvs[1]))
 					{
-					case -1:
-						fprintf(stderr, "Fork Failed\n");
-						exit(-1);
-
-					case 0: //the grandchild process,writing to the pipe
-
-						close(1);
-						dup2(file_pipes[1], 1);
-						close(file_pipes[1]);
-						close(file_pipes[0]);
-						execvp(argvs[0], argvs);
-						printf("Command not found in PATH\n");
-						exit(0);
-
-					default: //the child process, reading from the pipe
-
-						close(0);
-						dup(file_pipes[0]);
-						close(file_pipes[0]);
-						close(file_pipes[1]);
-						execvp(command[0], command);
-						printf("Command not found in PATH\n");
-						exit(0);
+						result = ERROR_WRONG_PARAMETER;
+						fprintf(stderr, "Error wrong parameter");
+					}
+					else
+					{
+						result = OK;
 					}
 				}
-			default:
-				waitpid(child, &stat_val, 0);
-				if (!WIFEXITED(stat_val))
-					printf("Child terminated abnormally\n");
 			}
-			continue;
+			else
+			{
+				result = CALL_(argc);
+				switch (result)
+				{
+				case ERROR_FORK:
+					fprintf(stderr, "Error fork");
+					exit(ERROR_FORK);
+				case ERROR_COMMAND:
+					fprintf(stderr, "Error command");
+					break;
+				case ERROR_IN:
+					fprintf(stderr, "Error in redirection");
+					break;
+				case ERROR_OUT:
+					fprintf(stderr, "Error out redirection");
+					break;
+				case ERROR_MISS_PARAMETER:
+					fprintf(stderr, "Error redirect parameters");
+					break;
+				case ERROR_PIPE:
+					fprintf(stderr, "Error pipe");
+					break;
+				case ERROR_PIPE_MISS_PARAMETER:
+					fprintf(stderr, "Error pipe parameters");
+					break;
+				}
+			}
 		}
-
-		/* 外部命令 */
-		pid_t pid = fork();
-		if (pid == 0)
-		{
-			/* 子进程 */
-			execvp(argvs[0], argvs);
-			/* execvp失败 */
-			return 255;
-		}
-		/* 父进程 */
-		wait(NULL);
 	}
 }
